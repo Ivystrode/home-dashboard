50f5fb9cc7a873d1b3c88e7957966f1842978280
Ivystrode <benjamin200@hotmail.co.uk>
2022-04-16 12:18:46 +0100

    add mongodb atlas connection to flask app; use mongodb on frontend; preview functions for getting separate data

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,33 @@ from flask import Flask, json, request
 from flask_cors import CORS
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://192.168.1.155:27017') # set to env var for completion. currently running on secondary machine (LMT-Lap-3)
-db = client['testData']
-collection = db.test_collection
+import os, sys
+from dotenv import load_dotenv
+load_dotenv()
+
+# ==========MONGODB HOSTED ON LMT-DESKTOP-2==========
+# client = MongoClient('mongodb://127.0.0.1:27017') # set to env var for completion. currently running on secondary machine (LMT-Lap-3)
+# db = client['test']
+# collection = db.main_collection
+
+# ==========MONGODB HOSTED ON ATLAS==========
+# db_to_connect_to = 'testdb'
+db_pwd = os.getenv("mongo_db_pwd")
+
+# connection_string = f'mongodb+srv://main:{db_pwd}@cluster0.4yhee.mongodb.net/{db_to_connect_to}?retryWrites=true&w=majority'
+connection_string = f'mongodb+srv://main:{db_pwd}@cluster0.4yhee.mongodb.net'
+client = MongoClient(connection_string, serverSelectionTimeoutMS=5000) 
+
+db = client['testdb']
+collection = db.testcol
+
+
+# ==========FLASK API==========
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
+cors = CORS(app, resources={r'/*': {"origins": '*'}})
+app.config['CORS_HEADER'] = 'Content-Type: application/json'
 
 @app.route("/")
 def hello():
@@ -17,6 +38,7 @@ def hello():
 def get_data():
     try:
         db_data = collection.find()
+        print(db_data)
         response = []
         for item in db_data:
             item['_id'] = str(item['_id'])
@@ -27,6 +49,20 @@ def get_data():
     except Exception as e:
         return e
     
+# @app.route("/get/graphs", methods=['GET'])
+# def get_graphs():
+#     try:
+#         graph_data = graphs.find()
+#         response = []
+#         for item in graph_data:
+#             item['_id'] = str(item['_id'])
+#             response.append(item)
+            
+#         return json.dumps(response)
+    
+#     except Exception as e:
+#         return e
+    
 @app.route("/post", methods=['POST'])
 def post_data():
     ins_data = request.get_json()
@@ -34,8 +70,10 @@ def post_data():
     try:
         try:
             data = collection.insert_many(ins_data)
+            print("INSERT MANY")
         except:
             data = collection.insert_one(ins_data)
+            print("INSERT ONE")
             
         print("inserting:")
         print(data)
@@ -43,4 +81,45 @@ def post_data():
     
     except Exception as e:
         return json.dumps(e)
+    
+# @app.route("/post/detections", methods=['POST'])
+# def post_detection():
+#     ins_data = request.get_json()
+    
+#     try:
+#         try:
+#             data = detections.insert_many(ins_data)
+#             print("INSERT MANY")
+#         except:
+#             data = detections.insert_one(ins_data)
+#             print("INSERT ONE")
+            
+#         print("inserting:")
+#         print(data)
+#         return str(data)
+    
+#     except Exception as e:
+#         return json.dumps(e)
+
+# @app.route("/post/graphs/{graph_type}", methods=['POST'])
+# def post_detection(graph_type):
+    """
+    Can we use a parameter to determine which graph ie temp/co2 to post the data to?
+    """
+#     ins_data = request.get_json()
+    
+#     try:
+#         try:
+#             data = detections.insert_many(ins_data)
+#             print("INSERT MANY")
+#         except:
+#             data = detections.insert_one(ins_data)
+#             print("INSERT ONE")
+            
+#         print("inserting:")
+#         print(data)
+#         return str(data)
+    
+#     except Exception as e:
+#         return json.dumps(e)
         
--- backend/post.py
+++ backend/post.py
@@ -1,7 +1,11 @@
-import json, requests 
+import json, requests
+ 
+global posturl
+posturl = "http://localhost:5000/post"
 
+test_data = {"co2": {"time": "19:40", "level": 25566}, "nothing": {"wow":"working"}}
 
-def send(posturl):
+def send_db_file(posturl):
     with open("db.json") as sendfile:
         data = json.load(sendfile)
     
@@ -10,8 +14,15 @@ def send(posturl):
     
 def get():
     d = requests.get("http://localhost:5000/get")
+    print(d)
     print(d.content)
 
+def add_to_graph_data(data):
+    
+    p = requests.post(posturl, json=data)
+    print(p)
+
 if __name__ == '__main__':
-    send("http://localhost:5000/post")
-    # get()
+    # send_db_file(posturl)
+    get()
+    # add_to_graph_data(test_data)
